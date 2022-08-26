0db378a6a44f7e43a21a5c6d3c8dff2b66b93dab
Alper.Arslan <alper.arslan663@gmail.com>
2022-08-27 01:57:32 +0300

    Category entity added as an example of ManyToMany.

--- src/main/java/com/alperArslan/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/alperArslan/spring5recipeapp/domain/Recipe.java
@@ -15,8 +15,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
     @Lob
@@ -25,6 +23,10 @@ public class Recipe {
     private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -120,5 +122,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
