a056317e26993bab7263456899e7d4f517e7c0f9
Alper.Arslan <alper.arslan663@gmail.com>
2022-08-27 15:01:55 +0300

    Added findByDescription to CategoryRepository and UnitOfMeasureRepository.

--- src/main/java/com/alperArslan/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/alperArslan/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.alperArslan.spring5recipeapp.controllers;
 
+import com.alperArslan.spring5recipeapp.domain.Category;
+import com.alperArslan.spring5recipeapp.domain.UnitOfMeasure;
+import com.alperArslan.spring5recipeapp.repositories.CategoryRepository;
+import com.alperArslan.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"","/","index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("hi");
+
+        System.out.println("category id " + categoryOptional.get().getId());
+        System.out.println("uom id " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/alperArslan/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/alperArslan/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package com.alperArslan.spring5recipeapp.repositories;
 import com.alperArslan.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
