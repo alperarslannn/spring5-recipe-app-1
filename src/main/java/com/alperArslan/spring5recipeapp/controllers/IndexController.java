037a11e2acc24e25a928e32b642f6f3ead3e6233
Alper.Arslan <alper.arslan663@gmail.com>
2022-08-27 16:42:20 +0300

    Bootstrap data is added for recipes and index page is prepared. RecipeService is created.

--- src/main/java/com/alperArslan/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/alperArslan/spring5recipeapp/controllers/IndexController.java
@@ -1,34 +1,24 @@
 package com.alperArslan.spring5recipeapp.controllers;
 
-import com.alperArslan.spring5recipeapp.domain.Category;
-import com.alperArslan.spring5recipeapp.domain.UnitOfMeasure;
-import com.alperArslan.spring5recipeapp.repositories.CategoryRepository;
-import com.alperArslan.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import com.alperArslan.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
 @Controller
 public class IndexController {
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("hi");
-
-        System.out.println("category id " + categoryOptional.get().getId());
-        System.out.println("uom id " + unitOfMeasureOptional.get().getId());
-
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
--- src/main/java/com/alperArslan/spring5recipeapp/repositories/RecipeRepository.java
+++ src/main/java/com/alperArslan/spring5recipeapp/repositories/RecipeRepository.java
@@ -4,5 +4,4 @@ import com.alperArslan.spring5recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecipeRepository extends CrudRepository<Recipe,Long> {
-
 }
