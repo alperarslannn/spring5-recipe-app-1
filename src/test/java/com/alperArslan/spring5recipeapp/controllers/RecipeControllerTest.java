5ea2d9a53ec8e7a6a2874a65f3235b2692178ba3
Alper.Arslan <alper.arslan663@gmail.com>
2022-08-31 22:17:23 +0300

    RecipeController, RecipeControllerTest and html file for Recipe page are added .

---
+++ src/test/java/com/alperArslan/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,65 @@
+package com.alperArslan.spring5recipeapp.controllers;
+
+import com.alperArslan.spring5recipeapp.domain.Recipe;
+import com.alperArslan.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+class RecipeControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @InjectMocks
+    RecipeController controller;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void showById() {
+    }
+
+    @Test
+    public void getRecipe() throws Exception{
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+
+    @Test
+    public void testGetRecipe() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
